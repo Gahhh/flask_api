d1d20554eb35f1fd3335eff08d3909cac765bd41
Yu Liang <liangyusam@gmail.com>
2022-03-27 23:02:30 +1100

    q4

--- service.py
+++ service.py
@@ -1,7 +1,7 @@
 import json
 
 from flask import Flask, request, jsonify
-from flask_restx import Resource, Api
+from flask_restx import Resource, Api, fields
 from flask_sqlalchemy import SQLAlchemy
 import requests
 import time
@@ -12,6 +12,8 @@ import re
 # Initialise the flask framework
 # Create a new db with sqlAlchemy track notification disabled
 # --------------------------------------------------------------------------------------------------
+default_host = '127.0.0.1'
+default_port = 5000
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///z3457800.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -38,6 +40,8 @@ class ActorsInfo(db.Model):
 # --------------------------------------------------------------------------------------------------
 # API for Q1
 # --------------------------------------------------------------------------------------------------
+
+# ------Utility Functions---------------------------------------------------------------------------
 def request_data(url):
     try:
         data = requests.get(url).json()
@@ -62,10 +66,12 @@ def get_show_list(id):
 
 @api.route('/actors',
            doc={'params': {'name': 'Actors_name, eg: Brad Pitt'},
-                'responses': {200: 'OK', 201: 'Created',
-                              400: 'Bad Request', 403: 'Forbidden',
-                              404: 'Not Found'}
+                'responses': {200: 'OK', 201: 'Created', 403: 'Forbidden', 404: 'Not Found'}
 })
+# ------------------------------------------------------------------------------------------------
+
+# -----Main API function--------------------------------------------------------------------------
+
 class Actors(Resource):
     def post(self):
         actor = request.args.get('name')
@@ -97,7 +103,7 @@ class Actors(Resource):
                 pack = {'id': ac_info['id'],
                         'last-update': str(now),
                         '_links': {'self': {
-                            'href': 'http://127.0.0.1:5000/actors/{}'.format(ac_info['id'])
+                            'href': f"http://{default_host}:{default_port}/actors/{ac_info['id']}"
                         }}}
                 return pack, 201
             else:
@@ -105,7 +111,77 @@ class Actors(Resource):
         else:
             return {'message': 'The actor is not found'}, 404
 
+# --------------------------------------------------------------------------------------------------
+# API for Q2
+# --------------------------------------------------------------------------------------------------
+resource_fields = api.model('Resource', {
+    "name": fields.String(example="Jones Smith"),
+    "country": fields.String(example="America"),
+    "birthday": fields.String(example="1979-07-17"),
+    "deathday": fields.String(example="2021-04-24"),
+    "shows": fields.List(fields.String,example=['show1', 'show2', 'show3'])
+})
+
+def create_response(actor):
+    show_list = list(json.loads(actor.show).values())
+    links = {'self':{'href': f"http://{default_host}:{default_port}/actors/{actor.id}"}}
+    if ActorsInfo.query.get(int(actor.id) - 1):
+        prev = f"http://{default_host}:{default_port}/actors/{int(actor.id)-1}"
+        links['previous'] = {'href': prev}
+    if ActorsInfo.query.get(int(actor.id) + 1):
+        next = f"http://{default_host}:{default_port}/actors/{int(actor.id)+1}"
+        links['next'] = {'href': next}
+    pack = {'id': actor.id, 'last-update': actor.last_update,
+            'name': actor.name, 'country': actor.country,
+            'birthday': actor.birthday, 'deathday': actor.deathday,
+            'shows': show_list,
+            '_links': links}
+    return pack
+
+@api.route('/actors/<int:id>', methods=['DELETE', 'GET', 'PATCH'],
+           doc={'params': {'id': "Actor's id, eg: 2"},
+                'responses': {200: 'OK', 400: 'Bad Request', 404: 'Not Found'}
+})
+class Actors(Resource):
+    def get(self, id):
+        try:
+            int(id)
+            actor = ActorsInfo.query.get(id)
+            if not actor:
+                return {'message': 'The actor is not found'}, 404
+            pack = create_response(actor)
+            return pack, 200
+        except Exception:
+            return {'message': 'id can only be a number'}, 400
+
+    def delete(self, id):
+        try:
+            int(id)
+            actor = ActorsInfo.query.get(id)
+            if not actor:
+                return {'message': 'The actor is not in database'}, 404
+            db.session.delete(actor)
+            db.session.commit()
+            return {'message': f'The actor with id {id} was removed from the database!',
+                    'id': id}, 200
+        except Exception:
+            return {'message': 'id can only be a number'}, 400
+
+    @api.doc(body=resource_fields)
+    def patch(self, id):
+        try:
+            int(id)
+            actor = ActorsInfo.query.get(id)
+            if not actor:
+                return {'message': 'The actor is not found'}, 404
+            pack = create_response(actor)
+            return pack, 200
+        except Exception:
+            return {'message': 'id can only be a number'}, 400
+
+
+
 
 if __name__ == '__main__':
     db.create_all()
-    app.run(debug=True)
+    app.run(host=default_host, port=default_port, debug=True)
